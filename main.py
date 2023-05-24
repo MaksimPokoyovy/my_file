4395662aab98b6a49d29c1c830ddf4683eae1d80
Maksim <Maksim_slonim1984@mail.ru>
2023-05-24 22:41:42 +0300

    домашка

---
+++ main.py
@@ -0,0 +1,65 @@
+# This is a sample Python script.
+
+##Новый файл
+
+#Дз на четверг (Ivanov_Lesson_13.py)
+# Создайте новый проект.
+# С помощь консоли Git bush (или любой другой) создайте локальный репозиторий в папке проекта (git init).
+# В локальном репозитории добавьте новый текстовый файл.
+# Затем создайте новую ветку и измените в ней текстовый файл.
+# После чего соедините ветки.
+# Локальный репозиторий опубликуйте на GitHub с помощью консоли GitBush или с помощью PyCharm через Share on GitHub
+#Сбрасываем мне ссылку на проект на GitHub
+#
+# def calculate():
+# operation = input('''
+# Please type in the math operation you would like to complete:
+# + for addition
+# - for subtraction
+# * for multiplication
+# / for division
+# ''')
+# number_1 = int(input('Please enter the first number: '))
+# number_2 = int(input('Please enter the second number: '))
+# if operation == '+':
+#     print('{} + {} = '.format(number_1, number_2))
+#     print(number_1 + number_2)
+# elif operation == '-':
+#     print('{} - {} = '.format(number_1, number_2))
+#     print(number_1 - number_2)
+# elif operation == '*':
+#     print('{} * {} = '.format(number_1, number_2))
+#     print(number_1 * number_2)
+# elif operation == '/':
+#     print('{} / {} = '.format(number_1, number_2))
+#     print(number_1 / number_2)
+# else:
+#     print('You have not typed a valid operator, please run the program again.')
+# # Добавление функции again() в calculate()
+# again()
+# def again():
+#     calc_again = input('''
+# Do you want to calculate again?
+# Please type Y for YES or N for NO.
+# ''')
+# if calc_again.upper() == 'Y':
+#     calculate()
+# elif calc_again.upper() == 'N':\
+#     print('See you later.')
+# else: again()
+# #calculate()
+#
+# # def calculater():
+# #     a = float(input('Первое число: '))
+#     b = float(input('Второе число: '))
+# #     sign = str(input('Введите операцию (для выхода введите 0): '))
+#     if sign == '0': break
+#
+#     if sign == '+': print(a+b)
+#     elif sign == '-': print(a-b)
+#     elif sign == '*': print(a * b)
+#     elif sign == '/':
+#         if b != 0:print(a / b)
+#         else: print('На ноль делить нельзя!')
+#     else:
+#         print('Такой операции нет!')
